86f6105769dda57b89fc43bb24d09add98a8f68d
Anshu Singh <anshusingh7738@gmail.com>
2024-03-31 00:37:29 +0530

    update: working on Age details Box.

--- app/src/main/java/com/example/agecalculator/screens/AgeCalculator.kt
+++ app/src/main/java/com/example/agecalculator/screens/AgeCalculator.kt
@@ -10,7 +10,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,6 +28,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,9 +54,12 @@ fun AgeCalculator() {
             // Header Text
             CustomText(
                 text = "Age",
-                textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 22.sp, textAlign = TextAlign.Center),
-                modifier = Modifier.
-                fillMaxWidth(),
+                textStyle = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp,
+                    textAlign = TextAlign.Center
+                ),
+                modifier = Modifier.fillMaxWidth(),
                 color = Color.Black,
             ) {
 
@@ -58,63 +72,94 @@ fun AgeCalculator() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(start = 12.dp, end = 12.dp, bottom = 12.dp, top = 30.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+                CustomText(
+                    text = "Date of Birth",
+                    textStyle = TextStyle(
+                        fontWeight = FontWeight.Normal, fontSize = 20.sp,
+                        textAlign = TextAlign.Start
+                    ),
+                    modifier = Modifier.weight(1f),
+                    color = Color.Black
 
                 ) {
-                    CustomText(
-                        text = "Date of Birth",
-                        textStyle = TextStyle(
-                            fontWeight = FontWeight.Normal, fontSize = 20.sp,
-                            textAlign = TextAlign.Start
-                        ),
-                        modifier = Modifier.weight(1f),
-                        color = Color.Black
 
-                    ){
-
-                    }
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        DatePickerDateOfBirth()
-                    }
                 }
 
-                Spacer(modifier = Modifier.height(10.dp))
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    DatePickerDateOfBirth()
+                }
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
 
             // Today Date
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(12.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+
+                CustomText(
+                    text = "Today",
+                    textStyle = TextStyle(
+                        fontWeight = FontWeight.Normal, fontSize = 18.sp,
+                        textAlign = TextAlign.Start
+                    ),
+                    modifier = Modifier.weight(1f),
+                    color = Color.Black
 
                 ) {
 
-                    CustomText(
-                        text = "Today",
-                        textStyle = TextStyle(
-                            fontWeight = FontWeight.Normal, fontSize = 18.sp,
-                            textAlign = TextAlign.Start
-                        ),
-                        modifier = Modifier.weight(1f),
-                        color = Color.Black
-
-                    ){
-
-                    }
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        DatePickerDateOfBirth()
-                    }
                 }
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    DatePickerDateOfBirth()
+                }
+            }
 
 
             BoxLayout()
         }
 
     }
+}
+
+@Composable
+fun TopAppBarDemo() {
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+
+            CenterAlignedTopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Centered Top App Bar",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                IconButton(onClick = { /* do something */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Localized description"
+                    )
+                }
+            },
+        },
+    )
 }
 
 
